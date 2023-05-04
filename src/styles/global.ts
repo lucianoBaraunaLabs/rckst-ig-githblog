4edd6d6f5573ba013de2d5801177373c550ed11a
Luciano Baraúna <baraunaluciano@gmail.com>
2023-05-04 00:08:19 -0300

    chore: install fonts and configure theme

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: none;
+    }
+    &:focus {
+      outline: 0;
+    }
+    body {
+      font-family: ${theme.fontBase};
+      -webkit-font-smoothing: antialiased;
+    }
+  `}
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,122 @@
+export const defaultTheme = {
+  palette: {
+    black: '#000',
+    white: '#FFF',
+    blue: '#3294F8',
+    'base-title': '#E7EDF4',
+    'base-subtitle': '#C4D4E3',
+    'base-text': '#AFC2D4',
+    'base-span': '#7B96B2',
+    'base-label': '#3A536B',
+    'base-border': '#1C2F41',
+    'base-post': '#112131',
+    'base-profile': '#0B1B2B',
+    'base-background': '#071422',
+    'base-input': '#040F1A',
+  },
+  fontBase: "'Nunito', sans-serif",
+  fontSize: {
+    10: '0.625rem',
+    12: '0.75rem',
+    14: '0.875rem',
+    base: '1rem',
+    18: '1.125rem',
+    20: '1.25rem',
+    22: '1.375rem',
+    24: '1.5rem',
+    32: '2rem',
+    48: '3rem',
+  },
+  fontWeight: {
+    /** 700 */
+    bold: 700,
+    /** 400 */
+    regular: 400,
+  },
+  borderRadius: {
+    /** 1px */
+    px: '1px',
+    /** 0px */
+    0: '0px',
+    /** 6px */
+    1.5: '0.375rem',
+    /** 36px */
+    9: '2.25rem',
+    /** 44px */
+    11: '2.75rem',
+    /** 9999px */
+    circle: '9999px',
+  },
+  spacing: {
+    /** 1px */
+    px: '1px',
+    /** 0px */
+    0: '0px',
+    /** 2px */
+    0.5: '0.125rem',
+    /** 4px */
+    1: '0.25rem',
+    /** 6px */
+    1.5: '0.375rem',
+    /** 8px */
+    2: '0.5rem',
+    /** 10px */
+    2.5: '0.625rem',
+    /** 12px */
+    3: '0.75rem',
+    /** 14px */
+    3.5: '0.875rem',
+    /** 16px */
+    4: '1rem',
+    /** 20px */
+    5: '1.25rem',
+    /** 24px */
+    6: '1.5rem',
+    /** 28px */
+    7: '1.75rem',
+    /** 32px */
+    8: '2rem',
+    /** 36px */
+    9: '2.25rem',
+    /** 40px */
+    10: '2.5rem',
+    /** 44px */
+    11: '2.75rem',
+    /** 48px */
+    12: '3rem',
+    /** 56px */
+    14: '3.5rem',
+    /** 64px */
+    16: '4rem',
+    /** 80px */
+    20: '5rem',
+    /** 96px */
+    24: '6rem',
+    /** 112px */
+    28: '7rem',
+    /** 128px */
+    32: '8rem',
+    /** 144px */
+    36: '9rem',
+    /** 160px */
+    40: '10rem',
+    /** 176px */
+    44: '11rem',
+    /** 192px */
+    48: '12rem',
+    /** 208px */
+    52: '13rem',
+    /** 224px */
+    56: '14rem',
+    /** 240px */
+    60: '15rem',
+    /** 256px */
+    64: '16rem',
+    /** 288px */
+    72: '18rem',
+    /** 320px */
+    80: '20rem',
+    /** 384px */
+    96: '24rem',
+  },
+}
