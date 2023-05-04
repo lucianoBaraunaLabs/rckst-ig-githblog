e29091baeb7a68aa0ddd0f60d5906f3a16746a40
Luciano Baraúna <baraunaluciano@gmail.com>
2023-05-03 23:32:52 -0300

    chore: configure alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from '@/styles/global'
+import { defaultTheme } from '@/styles/themes/default'
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
       <h1>Github blog</h1>
-    </>
+    </ThemeProvider>
   )
 }
 
