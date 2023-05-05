15bff99dabc0d74d2d88f32bda7ffda773e577d5
Luciano Baraúna <baraunaluciano@gmail.com>
2023-05-04 23:45:20 -0300

    feat(Router): implemented redirect

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Post } from './pages/Post'
 import { DefaultLayout } from './layouts/DefaultLayout'
@@ -9,6 +9,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/post/:idPost" element={<Post />} />
+        <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
   )
