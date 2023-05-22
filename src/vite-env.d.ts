3dbd91f76b2c8fb07b606b48909ab9d39a76c9f4
Luciano Baraúna <baraunaluciano@gmail.com>
2023-05-22 02:24:03 -0300

    chore: create env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GITHUB_USERNAME: string
+  readonly VITE_GITHUB_REPONAME: string
+}
+
+// eslint-disable-next-line no-unused-vars
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
