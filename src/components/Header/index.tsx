fd1111fa0f02b4125c2231df876665e7a724052e
Luciano Baraúna <lucianobaraunadev@gmail.com>
2025-01-17 22:23:03 -0300

    feat: refactor components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,63 +1,10 @@
-import * as S from './styles.ts'
-import {
-  ArrowSquareOut,
-  Buildings,
-  CaretLeft,
-  GithubLogo,
-  Users,
-} from 'phosphor-react'
-import { HeaderProps } from './types.ts'
-import { useGithubApi } from '@/contexts/GithubContext.tsx'
+import { HeaderContainer } from './styles'
+import logoSrc from '@/assets/images/logo.svg'
 
-export function Header({ isHeaderPost }: HeaderProps) {
-  const { infoUser, loading } = useGithubApi()
-
+export function Header() {
   return (
-    <S.Wrapper isHeaderPost={isHeaderPost}>
-      {loading.infoUser && <>carregando...</>}
-      {!isHeaderPost ? (
-        <S.ImgProfile src={infoUser.avatarUrl} alt="Imagem aqui" />
-      ) : (
-        <S.WrapperPost>
-          <S.Link to="/">
-            <CaretLeft />
-            VOLTAR
-          </S.Link>
-          <S.Anchor href="">
-            VER NO GITHUB <ArrowSquareOut weight="bold" />
-          </S.Anchor>
-        </S.WrapperPost>
-      )}
-      <S.Content>
-        <S.Title>
-          {!isHeaderPost ? (
-            <>
-              {infoUser.name}
-              <S.Anchor href={infoUser.htmlUrl}>
-                GITHUB <ArrowSquareOut weight="bold" />
-              </S.Anchor>
-            </>
-          ) : (
-            <>Título do post aqui</>
-          )}
-        </S.Title>
-
-        {!isHeaderPost && <S.Text>{infoUser.bio}</S.Text>}
-        <S.List>
-          <S.ListItem>
-            <GithubLogo weight="bold" />
-            <p>{infoUser.login}</p>
-          </S.ListItem>
-          <S.ListItem>
-            <Buildings weight="bold" />
-            <p>{infoUser.company}</p>
-          </S.ListItem>
-          <S.ListItem>
-            <Users weight="bold" />
-            <p>{infoUser.followers} seguidores</p>
-          </S.ListItem>
-        </S.List>
-      </S.Content>
-    </S.Wrapper>
+    <HeaderContainer>
+      <img src={logoSrc} alt="" />
+    </HeaderContainer>
   )
 }
